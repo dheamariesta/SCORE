7fb02e10c2c12dc96f474007ecda210b1f2d2954
dheamariesta <dhea23mar@gmail.com>
2017-06-24 17:50:57 +0800

    cannot see secret pages without logging in

--- score-bakery/routes/productRoutes.js
+++ score-bakery/routes/productRoutes.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import productController from '../controllers/productController';
-
+import userController from '../controllers/userController'
 const router = express.Router();
 
+function isLoggedIn (req, res, next) {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login')
+}
 
-router.get('/product/eggtart', (req, res) => {
+router.get('/product/eggtart', isLoggedIn, (req, res) => {
   //console.log(req.user);
   res.render('eggtart', {
     message: req.flash('loginMessage')
   })
-});
+}, userController.getUserDetails);
 
 
 //POST method for user login
--- score-bakery/routes/shoppingcartRoutes.js
+++ score-bakery/routes/shoppingcartRoutes.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import productController from '../controllers/productController';
-
+import userController from '../controllers/userController'
 const router = express.Router();
 
+function isLoggedIn (req, res, next) {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login')
+}
 
-router.get('/shoppingcart', (req, res) => {
+router.get('/shoppingcart', isLoggedIn, (req, res) => {
   //console.log(req.user);
   res.render('shoppingcart', {
     message: req.flash('loginMessage')
   })
-});
+}, userController.getUserDetails);
 
 
 //POST method for user login
